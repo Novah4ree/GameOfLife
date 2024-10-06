5b94a50e1f1a9be2762e6dee4b931c563f178ac2
Novah4ree <kapereira@student.fullsail.edu>
2024-10-05 17:04:20 -0800

    created a window with two buttons

--- GameOfLife/Window.cpp
+++ GameOfLife/Window.cpp
@@ -2,5 +2,7 @@
 
 Window::Window() : wxFrame(nullptr, wxID_ANY, "COS1 Example", wxPoint(100, 100), wxSize(300, 200)) {
 
+	button1 = new wxButton(this, 1001, "Click Here",wxPoint(25,50), wxSize(75,25));
+	button2 = new wxButton(this, 1002, "Better Yet, Click Here", wxPoint(155, 50), wxSize(125, 25));
 
 }
--- GameOfLife/Window.h
+++ GameOfLife/Window.h
@@ -5,7 +5,8 @@
 class Window : public wxFrame
 {
 private:
-
+    wxButton* button1 = nullptr;
+    wxButton* button2 = nullptr;
 public:
     Window();
 };
